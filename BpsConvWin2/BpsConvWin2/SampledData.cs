c3cedba2a9e0f2f12256da64c07679ebdb0fc4be
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2009-02-06 17:34:04 +0000

    BpsConvWin2

---
+++ BpsConvWin2/BpsConvWin2/SampledData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BpsConvWin2
+{
+    struct SampledData1channel
+    {
+        uint    sampleRate;
+        float[] samples;
+    }
+
+    class SampledDataAllChannels
+    {
+        List<SampledData1channel> channels;
+    }
+}
