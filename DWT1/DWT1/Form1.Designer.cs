e8a796fd9916940a4d206ca0ec59853fa2c09fa8
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2009-07-11 07:18:12 +0000

    dwt

---
+++ DWT1/DWT1/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace DWT1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBoxSource = new System.Windows.Forms.PictureBox();
+            this.pictureBoxDWTed = new System.Windows.Forms.PictureBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDWTed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxSource
+            // 
+            this.pictureBoxSource.Location = new System.Drawing.Point(16, 14);
+            this.pictureBoxSource.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBoxSource.Name = "pictureBoxSource";
+            this.pictureBoxSource.Size = new System.Drawing.Size(683, 321);
+            this.pictureBoxSource.TabIndex = 0;
+            this.pictureBoxSource.TabStop = false;
+            this.pictureBoxSource.MouseLeave += new System.EventHandler(this.pictureBoxSource_MouseLeave);
+            this.pictureBoxSource.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBoxSource_MouseMove);
+            this.pictureBoxSource.MouseEnter += new System.EventHandler(this.pictureBoxSource_MouseEnter);
+            // 
+            // pictureBoxDWTed
+            // 
+            this.pictureBoxDWTed.Location = new System.Drawing.Point(16, 349);
+            this.pictureBoxDWTed.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBoxDWTed.Name = "pictureBoxDWTed";
+            this.pictureBoxDWTed.Size = new System.Drawing.Size(683, 160);
+            this.pictureBoxDWTed.TabIndex = 1;
+            this.pictureBoxDWTed.TabStop = false;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(16, 516);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(100, 26);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "&SAVE";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(124, 516);
+            this.buttonLoad.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(100, 26);
+            this.buttonLoad.TabIndex = 3;
+            this.buttonLoad.Text = "&LOAD";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(232, 515);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.trackBar1.Maximum = 8;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(329, 53);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Value = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(584, 527);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(16, 592);
+            this.trackBar2.Maximum = 127;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(256, 53);
+            this.trackBar2.TabIndex = 7;
+            this.trackBar2.TickFrequency = 16;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(278, 603);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 657);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.pictureBoxDWTed);
+            this.Controls.Add(this.pictureBoxSource);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "DWT";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDWTed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxSource;
+        private System.Windows.Forms.PictureBox pictureBoxDWTed;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ DWT1/DWT1/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DWT1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBoxSource.Image = null;
+            pictureBoxDWTed.Image = null;
+            if (!LoadSignal(SIGNAL_FILENAME))
+            {
+                PrepareSourceData();
+            }
+            TrackbarMagnitudeUpdated();
+            TrackbarOffsetUpdated();
+            UpdateGui();
+        }
+
+        private const int SRC_W = 512;
+        private const int SRC_H = 257;
+        private const int DWT_H = 256;
+        private const int SRC_HALF_H = 128;
+        private const string SIGNAL_FILENAME = "sourceSignal.txt";
+        private float[] sourceSignal = null;
+        private int offset = 0;
+
+        private void PrepareSourceData()
+        {
+            sourceSignal = new float[SRC_W];
+            for (int x = 0; x < SRC_W; ++x)
+            {
+                sourceSignal[x] = (float)Math.Sin(x * 0.04f);
+            }
+        }
+
+        private void SaveSignal(string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine("1");
+                sw.WriteLine(sourceSignal.Length);
+
+                for (int i = 0; i < sourceSignal.Length; ++i)
+                {
+                    sw.WriteLine(sourceSignal[i]);
+                }
+            }
+        }
+
+        private bool LoadSignal(string path)
+        {
+            bool ret = false;
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string version = sr.ReadLine().Trim();
+                    if (0 != version.CompareTo("1"))
+                    {
+                        return false;
+                    }
+
+                    sourceSignal = new float[SRC_W];
+                    int length = System.Int32.Parse(sr.ReadLine().Trim());
+                    if (SRC_W < length)
+                    {
+                        length = SRC_W;
+                    }
+
+                    for (int i = 0; i < length; ++i)
+                    {
+                        sourceSignal[i] = (float)System.Double.Parse(sr.ReadLine().Trim());
+                    }
+                }
+                ret = true;
+            }
+            catch
+            {
+            }
+            return ret;
+        }
+
+        private void UpdateGui()
+        {
+            UpdatePictureBoxSource();
+            UpdateDwt();
+        }
+
+        private Point prevXY;
+
+        private void pictureBoxSource_MouseEnter(object sender, EventArgs e)
+        {
+            prevXY = new Point(-1, -1);
+        }
+
+        private void pictureBoxSource_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.None)
+            {
+                return;
+            }
+            if (e.X < 0 || SRC_W <= e.X ||
+                e.Y < 0 || SRC_H <= e.Y)
+            {
+                return;
+            }
+
+            if (prevXY.X < 0)
+            {
+                prevXY = new Point(e.X, e.Y);
+                return;
+            }
+
+            Point from = new Point(prevXY.X, prevXY.Y);
+            Point to = new Point(e.X, e.Y);
+            float katamuki = 0;
+            if (e.X < prevXY.X)
+            {
+                from = new Point(e.X, e.Y);
+                to = new Point(prevXY.X, prevXY.Y);
+            }
+
+            if (from.X != to.X)
+            {
+                katamuki = ((float)to.Y - from.Y) / (to.X - from.X);
+            }
+
+            for (int i = 0; i <= to.X - from.X; ++i)
+            {
+                int x = from.X + i;
+                float y = (float)from.Y + katamuki * i;
+                sourceSignal[x] = (SRC_HALF_H - y) / SRC_HALF_H;
+            }
+            prevXY = new Point(e.X, e.Y);
+            UpdateGui();
+        }
+
+        private void pictureBoxSource_MouseLeave(object sender, EventArgs e)
+        {
+            prevXY = new Point(-1, -1);
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveSignal(SIGNAL_FILENAME);
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            LoadSignal(SIGNAL_FILENAME);
+            UpdateGui();
+        }
+
+        private void UpdatePictureBoxSource()
+        {
+            Bitmap bmp = new Bitmap(SRC_W, SRC_H);
+            Graphics g = Graphics.FromImage(bmp);
+            g.FillRectangle(new SolidBrush(Color.FromArgb(0xff, 0xff, 0xff, 0xff)), 0, 0, SRC_W, SRC_H);
+
+            PointF[] points = new PointF[(sourceSignal.Length - 1) * 2];
+
+            for (int x = 0; x < sourceSignal.Length - 1; ++x)
+            {
+                points[x * 2] = new PointF(x, -sourceSignal[x] * SRC_HALF_H + SRC_HALF_H);
+                points[x * 2 + 1] = new PointF(x + 1, -sourceSignal[x + 1] * SRC_HALF_H + SRC_HALF_H);
+            }
+            g.DrawLines(new Pen(Color.Black), points);
+
+            if (null != pictureBoxSource.Image)
+            {
+                pictureBoxSource.Image.Dispose();
+            }
+            pictureBoxSource.Image = bmp;
+        }
+
+        private int magnitude = 256;
+
+        private void UpdateDwt()
+        {
+            Bitmap bmp = new Bitmap(SRC_W, DWT_H);
+            Graphics g = Graphics.FromImage(bmp);
+            g.FillRectangle(new SolidBrush(Color.FromArgb(0xff, 0, 0, 0)), 0, 0, SRC_W, DWT_H);
+
+            RectangleF[] rects = new RectangleF[sourceSignal.Length * 3];
+            float[] dwtData = new float[sourceSignal.Length*9];
+            sourceSignal.CopyTo(dwtData, 0);
+
+            Color[] colors = new Color[sourceSignal.Length * 3];
+
+            int readColumn = 0;
+            int writeColumn = sourceSignal.Length;
+            float y = 0;
+            float h = 16.0f;
+            int posR = 0;
+
+            for (int j = sourceSignal.Length / 2; 2 <= j; j /= 2)
+            {
+                float w = sourceSignal.Length / j;
+
+                for (int i = 0; i < j; ++i)
+                {
+                    float a = (dwtData[readColumn + i * 2] + dwtData[readColumn + i * 2 + 1]) * 0.5f;
+                    float d = dwtData[readColumn + i * 2] - a;
+                    dwtData[writeColumn + i] = a;
+                    dwtData[writeColumn + j + i] = d;
+
+                    rects[posR] = new RectangleF(i * w, y, w, h);
+
+                    int r = (int)((0 < d) ? d * magnitude : 0);
+                    if (255 < r) { r = 255; }
+                    int b = (int)((d < 0) ? -d * magnitude : 0);
+                    if (255 < b) { b = 255; }
+                    colors[posR++] = Color.FromArgb(b, 0, r);
+                }
+
+                for (int i = j * 2; i < sourceSignal.Length; ++i)
+                {
+                    dwtData[writeColumn + i] = dwtData[writeColumn - sourceSignal.Length + i];
+                }
+
+                readColumn += sourceSignal.Length;
+                writeColumn += sourceSignal.Length;
+                y += h;
+            }
+
+            for (int i = 0; i < posR; ++i)
+            {
+                g.FillRectangle(new SolidBrush(colors[i]), rects[i]);
+            }
+
+            if (null != pictureBoxDWTed.Image)
+            {
+                pictureBoxDWTed.Image.Dispose();
+            }
+            pictureBoxDWTed.Image = bmp;
+        }
+
+        private void TrackbarMagnitudeUpdated()
+        {
+            magnitude = (int)Math.Pow(2, trackBar1.Value + 8);
+            label1.Text = string.Format("{0} x", magnitude);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            TrackbarMagnitudeUpdated();
+            UpdateGui();
+        }
+
+        private void TrackbarOffsetUpdated()
+        {
+            offset = (int)trackBar2.Value;
+            label2.Text = string.Format("offset = {0}", offset);
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            TrackbarOffsetUpdated();
+            UpdateGui();
+        }
+    }
+}
