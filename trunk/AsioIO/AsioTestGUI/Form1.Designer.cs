bd90ddecd3072a6d88354eea421566177f4d8e9e
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2009-08-11 14:33:51 +0000

    Pulse5

--- trunk/AsioIO/AsioTestGUI/Form1.Designer.cs
+++ trunk/AsioIO/AsioTestGUI/Form1.Designer.cs
@@ -39,12 +39,16 @@
             this.buttonStart = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.textBoxFilePath = new System.Windows.Forms.TextBox();
             this.buttonBrowse = new System.Windows.Forms.Button();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
             this.buttonStop = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.numericUpDownPulseCount = new System.Windows.Forms.NumericUpDown();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPulseCount)).BeginInit();
             this.SuspendLayout();
             // 
             // listBoxDrivers
@@ -53,7 +57,7 @@
             this.listBoxDrivers.ItemHeight = 15;
             this.listBoxDrivers.Location = new System.Drawing.Point(6, 21);
             this.listBoxDrivers.Name = "listBoxDrivers";
-            this.listBoxDrivers.Size = new System.Drawing.Size(413, 109);
+            this.listBoxDrivers.Size = new System.Drawing.Size(413, 64);
             this.listBoxDrivers.TabIndex = 0;
             // 
             // groupBox1
@@ -62,17 +66,17 @@
             this.groupBox1.Controls.Add(this.listBoxDrivers);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(425, 176);
+            this.groupBox1.Size = new System.Drawing.Size(425, 123);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "1. Select ASIO driver";
+            this.groupBox1.Text = "1. Select ASIO driver (only when 2 or more devices exist)";
             // 
             // buttonLoadDriver
             // 
             this.buttonLoadDriver.Enabled = false;
-            this.buttonLoadDriver.Location = new System.Drawing.Point(6, 136);
+            this.buttonLoadDriver.Location = new System.Drawing.Point(6, 91);
             this.buttonLoadDriver.Name = "buttonLoadDriver";
-            this.buttonLoadDriver.Size = new System.Drawing.Size(184, 33);
+            this.buttonLoadDriver.Size = new System.Drawing.Size(184, 25);
             this.buttonLoadDriver.TabIndex = 1;
             this.buttonLoadDriver.Text = "Load Driver";
             this.buttonLoadDriver.UseVisualStyleBackColor = true;
@@ -87,7 +91,7 @@
             this.groupBox2.Size = new System.Drawing.Size(425, 400);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "2. Select IO channels to use";
+            this.groupBox2.Text = "3. Select IO channels to use";
             // 
             // listBoxInput
             // 
@@ -135,15 +139,7 @@
             this.groupBox3.Size = new System.Drawing.Size(425, 56);
             this.groupBox3.TabIndex = 4;
             this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "3. Specify output file";
-            // 
-            // textBoxFilePath
-            // 
-            this.textBoxFilePath.Location = new System.Drawing.Point(6, 21);
-            this.textBoxFilePath.Name = "textBoxFilePath";
-            this.textBoxFilePath.Size = new System.Drawing.Size(293, 22);
-            this.textBoxFilePath.TabIndex = 0;
-            this.textBoxFilePath.Text = "REC.WAV";
+            this.groupBox3.Text = "4. Specify file to record";
             // 
             // buttonBrowse
             // 
@@ -154,6 +150,14 @@
             this.buttonBrowse.Text = "Browse...";
             this.buttonBrowse.UseVisualStyleBackColor = true;
             this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // textBoxFilePath
+            // 
+            this.textBoxFilePath.Location = new System.Drawing.Point(6, 21);
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.Size = new System.Drawing.Size(293, 22);
+            this.textBoxFilePath.TabIndex = 0;
+            this.textBoxFilePath.Text = "REC.WAV";
             // 
             // buttonStop
             // 
@@ -166,11 +170,44 @@
             this.buttonStop.UseVisualStyleBackColor = true;
             this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
             // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.numericUpDownPulseCount);
+            this.groupBox4.Location = new System.Drawing.Point(12, 141);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(419, 47);
+            this.groupBox4.TabIndex = 6;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "2. Pulse count";
+            // 
+            // numericUpDownPulseCount
+            // 
+            this.numericUpDownPulseCount.Location = new System.Drawing.Point(6, 19);
+            this.numericUpDownPulseCount.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownPulseCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownPulseCount.Name = "numericUpDownPulseCount";
+            this.numericUpDownPulseCount.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownPulseCount.TabIndex = 0;
+            this.numericUpDownPulseCount.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(449, 728);
+            this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.buttonStop);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.progressBar1);
@@ -178,11 +215,13 @@
             this.Controls.Add(this.buttonStart);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Pulse5";
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPulseCount)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -201,6 +240,8 @@
         private System.Windows.Forms.Button buttonBrowse;
         private System.Windows.Forms.TextBox textBoxFilePath;
         private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.NumericUpDown numericUpDownPulseCount;
     }
 }
 
