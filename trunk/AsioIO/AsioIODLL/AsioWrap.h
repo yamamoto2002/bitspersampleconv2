773d2513fcf0ad8a3ff19e47398262e807e8028b
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2009-08-11 23:05:43 +0000

    code cleanup

--- trunk/AsioIO/AsioIODLL/AsioWrap.h
+++ trunk/AsioIO/AsioIODLL/AsioWrap.h
@@ -1,60 +1,34 @@
 #ifndef H_AsioWrap
 #define H_AsioWrap
 
-int
-AsioWrap_getDriverNum(void);
+int  AsioWrap_getDriverNum(void);
+bool AsioWrap_getDriverName(int n, char *name_return, int size);
 
-bool
-AsioWrap_getDriverName(int n, char *name_return, int size);
-
-bool
-AsioWrap_loadDriver(int n);
-
-void
-AsioWrap_unloadDriver(void);
+bool AsioWrap_loadDriver(int n);
+void AsioWrap_unloadDriver(void);
 
 struct ASIOTimeStamp;
-
-double
-AsioTimeStampToDouble(ASIOTimeStamp &a);
+double AsioTimeStampToDouble(ASIOTimeStamp &a);
 
 struct ASIOSamples;
-double
-AsioSamplesToDouble(ASIOSamples &a);
+double AsioSamplesToDouble(ASIOSamples &a);
 
-int
-AsioWrap_setup(int sampleRate);
+int  AsioWrap_setup(int sampleRate);
+void AsioWrap_unsetup(void);
 
-void
-AsioWrap_unsetup(void);
+int AsioWrap_getInputChannelsNum(void);
+int AsioWrap_getOutputChannelsNum(void);
 
-int
-AsioWrap_getInputChannelsNum(void);
-int
-AsioWrap_getOutputChannelsNum(void);
+bool AsioWrap_getInputChannelName(int n, char *name_return, int size);
+bool AsioWrap_getOutputChannelName(int n, char *name_return, int size);
 
-bool
-AsioWrap_getInputChannelName(int n, char *name_return, int size);
+void AsioWrap_setOutput(int outputChannel, int *data, int samples);
+void AsioWrap_setInput(int inputChannel, int samples);
 
-bool
-AsioWrap_getOutputChannelName(int n, char *name_return, int size);
+int  AsioWrap_start(void);
+bool AsioWrap_run(void);
+void AsioWrap_stop(void);
 
-void
-AsioWrap_setOutput(int outputChannel, int *data, int samples);
-
-void
-AsioWrap_setInput(int inputChannel, int samples);
-
-int
-AsioWrap_start(void);
-
-bool
-AsioWrap_run(void);
-
-void
-AsioWrap_stop(void);
-
-void
-AsioWrap_getRecordedData(int inputChannel, int recordedData_return[], int samples);
+void AsioWrap_getRecordedData(int inputChannel, int recordedData_return[], int samples);
 
 #endif /* H_AsioWrap */
