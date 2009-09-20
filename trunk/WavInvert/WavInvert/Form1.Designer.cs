58dcf3e424e561a9c1065936333d6aae021a782e
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2009-09-20 18:05:54 +0000

    WavInvert

---
+++ trunk/WavInvert/WavInvert/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace WavInvert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonBrowseInput = new System.Windows.Forms.Button();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.buttonBrowseOutput = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.checkBoxInvert = new System.Windows.Forms.CheckBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBoxConsole = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonBrowseInput);
+            this.groupBox1.Controls.Add(this.textBoxInput);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(570, 57);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input File";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonBrowseOutput);
+            this.groupBox2.Controls.Add(this.textBoxOutput);
+            this.groupBox2.Location = new System.Drawing.Point(12, 170);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(569, 60);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output File";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(6, 21);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(447, 22);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // buttonBrowseInput
+            // 
+            this.buttonBrowseInput.Location = new System.Drawing.Point(459, 21);
+            this.buttonBrowseInput.Name = "buttonBrowseInput";
+            this.buttonBrowseInput.Size = new System.Drawing.Size(104, 23);
+            this.buttonBrowseInput.TabIndex = 1;
+            this.buttonBrowseInput.Text = "&Browse...";
+            this.buttonBrowseInput.UseVisualStyleBackColor = true;
+            this.buttonBrowseInput.Click += new System.EventHandler(this.buttonBrowseInput_Click);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(6, 20);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(459, 22);
+            this.textBoxOutput.TabIndex = 0;
+            // 
+            // buttonBrowseOutput
+            // 
+            this.buttonBrowseOutput.Location = new System.Drawing.Point(470, 20);
+            this.buttonBrowseOutput.Name = "buttonBrowseOutput";
+            this.buttonBrowseOutput.Size = new System.Drawing.Size(93, 23);
+            this.buttonBrowseOutput.TabIndex = 2;
+            this.buttonBrowseOutput.Text = "B&rowse...";
+            this.buttonBrowseOutput.UseVisualStyleBackColor = true;
+            this.buttonBrowseOutput.Click += new System.EventHandler(this.buttonBrowseOutput_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.checkBoxInvert);
+            this.groupBox3.Location = new System.Drawing.Point(12, 75);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(570, 89);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Effect";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 236);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(174, 23);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "&Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // checkBoxInvert
+            // 
+            this.checkBoxInvert.AutoSize = true;
+            this.checkBoxInvert.Location = new System.Drawing.Point(6, 21);
+            this.checkBoxInvert.Name = "checkBoxInvert";
+            this.checkBoxInvert.Size = new System.Drawing.Size(63, 19);
+            this.checkBoxInvert.TabIndex = 0;
+            this.checkBoxInvert.Text = "&Invert";
+            this.checkBoxInvert.UseVisualStyleBackColor = true;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // textBoxConsole
+            // 
+            this.textBoxConsole.Location = new System.Drawing.Point(12, 265);
+            this.textBoxConsole.Multiline = true;
+            this.textBoxConsole.Name = "textBoxConsole";
+            this.textBoxConsole.ReadOnly = true;
+            this.textBoxConsole.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxConsole.Size = new System.Drawing.Size(563, 204);
+            this.textBoxConsole.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 481);
+            this.Controls.Add(this.textBoxConsole);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "WavInvert";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonBrowseInput;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button buttonBrowseOutput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox checkBoxInvert;
+        private System.Windows.Forms.Button buttonStart;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBoxConsole;
+    }
+}
+
