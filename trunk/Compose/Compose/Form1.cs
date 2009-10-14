e9832f3c02126538b87e14b23586ba67cdea8244
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2009-10-14 15:01:09 +0000

    update compose create midi file

--- trunk/Compose/Compose/Form1.cs
+++ trunk/Compose/Compose/Form1.cs
@@ -10,9 +10,82 @@ using System.IO;
 
 namespace Compose
 {
+    class MidiWriter
+    {
+        public MidiWriter(BinaryWriter a)
+        {
+            bw = a;
+        }
+
+        BinaryWriter bw;
+
+        public void WriteByte(byte b)
+        {
+            bw.Write(b);
+        }
+
+        public void WriteBE4(int v)
+        {
+            bw.Write((byte)((v >> 24) & 0xff));
+            bw.Write((byte)((v >> 16) & 0xff));
+            bw.Write((byte)((v >> 8) & 0xff));
+            bw.Write((byte)(v & 0xff));
+        }
+
+        public void WriteBE2(short v)
+        {
+            bw.Write((byte)((v >> 8) & 0xff));
+            bw.Write((byte)(v & 0xff));
+        }
+
+        public static int CountMidiNumberBytes(ushort v) {
+            if (128 * 128 <= v)
+            {
+                return 3;
+            }
+            else if (128 <= v)
+            {
+                return 2;
+            }
+            else {
+                return 1;
+            }
+        }
+
+        public void WriteMidiNumber(ushort v)
+        {
+            if (128 * 128 <= v)
+            {
+                int v2 = v/128*128;
+                int v1 = v - v2 * 128*128;
+                v1 = v1 / 128;
+                int v0 = v - v2 * 128*128- v1 *128;
+
+                v2 += 129;
+                v1 += 129;
+
+                bw.Write((byte)v2);
+                bw.Write((byte)v1);
+                bw.Write((byte)v0);
+            }
+            else if (128 <= v)
+            {
+                int v1 = v / 128;
+                int v0 = v - v1 *128;
+
+                v1 += 129;
+
+                bw.Write((byte)v1);
+                bw.Write((byte)v0);
+            } else {
+                bw.Write((byte)v);
+            }
+        }
+    }
+
     enum NoteType
     {
-        Silence,
+        Silence = -1,
         C,
         CIS,
         D,
@@ -41,34 +114,61 @@ namespace Compose
             this.octave = octave;
             this.volume = volume;
         }
-    }
 
-    class BinaryWriterBE
-    {
-        public BinaryWriterBE(BinaryWriter a)
-        {
-            bw = a;
+        public int CountMidiBytes(ref int timeCursorRW) {
+            if (pitch == NoteType.Silence)
+            {
+                return 0;
+            }
+
+            int bytes =
+                MidiWriter.CountMidiNumberBytes((ushort)(offset - timeCursorRW))
+                + 3 +
+                MidiWriter.CountMidiNumberBytes((ushort)(length))
+                + 3;
+            timeCursorRW = offset + length;
+            return bytes;
         }
 
-        BinaryWriter bw;
+        // returns new time cursor
+        public int Write(int timeCursor, MidiWriter mw) {
+            if (pitch == NoteType.Silence)
+            {
+                return timeCursor;
+            }
 
-        public void WriteByte(byte b)
-        {
-            bw.Write(b);
-        }
+            // pitch
+            int p = (int)pitch + 0x3c;
+            switch (octave) {
+                case 1: p -= 12 * 3; break;
+                case 2: p -= 12 * 2; break;
+                case 3: p -= 12; break;
+                case 4: break;
+                case 5: p += 12; break;
+                case 6: p += 12 * 2; break;
+                case 7: p += 12 * 2; break;
+                case 8: p += 12 * 4; break;
+                default:
+                    System.Diagnostics.Debug.Assert(false);
+                    break;
+            }
 
-        public void WriteBE4(int v)
-        {
-            bw.Write((byte)((v >> 24) & 0xff));
-            bw.Write((byte)((v >> 16) & 0xff));
-            bw.Write((byte)((v >> 8) & 0xff));
-            bw.Write((byte)(v & 0xff));
-        }
+            mw.WriteMidiNumber((ushort)(offset - timeCursor));
+            mw.WriteByte(0x90);
 
-        public void WriteBE2(short v)
-        {
-            bw.Write((byte)((v >> 8) & 0xff));
-            bw.Write((byte)(v & 0xff));
+            System.Diagnostics.Debug.Assert(p <= 0x7f);
+            mw.WriteByte((byte)p);
+
+            System.Diagnostics.Debug.Assert(volume <= 0x7f);
+            mw.WriteByte((byte)volume);
+
+            // stop note
+            mw.WriteMidiNumber((ushort)(length));
+            mw.WriteByte(0x90);
+            mw.WriteByte((byte)p);
+            mw.WriteByte((byte)0);
+            
+            return offset + length;
         }
     }
 
@@ -92,6 +192,26 @@ namespace Compose
             noteList.Add(note);
         }
 
+        public void WriteMidiPart(BinaryWriter a) {
+            MidiWriter mw = new MidiWriter(a);
+
+            int timeCursor = 0;
+            foreach (Note n in noteList) {
+                timeCursor = n.Write(timeCursor, mw);
+            }
+        }
+
+        public int CountMidiBytes() {
+            int bytes = 0;
+            int timeCursor = 0;
+            foreach (Note n in noteList)
+            {
+                bytes += n.CountMidiBytes(ref timeCursor);
+            }
+
+            return bytes;
+        }
+
         System.Collections.Generic.List<Note> noteList;
     }
 
@@ -106,38 +226,39 @@ namespace Compose
         }
 
         public void WriteMidiHeader(BinaryWriter a) {
-            BinaryWriterBE bw = new BinaryWriterBE(a);
+            MidiWriter mw = new MidiWriter(a);
 
-            bw.WriteByte((byte)'M');
-            bw.WriteByte((byte)'T');
-            bw.WriteByte((byte)'h');
-            bw.WriteByte((byte)'d');
+            mw.WriteByte((byte)'M');
+            mw.WriteByte((byte)'T');
+            mw.WriteByte((byte)'h');
+            mw.WriteByte((byte)'d');
             
-            bw.WriteBE4(6);
-            bw.WriteBE2(1);
-            bw.WriteBE2(nTrack);
-            bw.WriteBE2(tempo);
+            mw.WriteBE4(6);
+            mw.WriteBE2(1);
+            mw.WriteBE2(nTrack);
+            mw.WriteBE2(tempo);
         }
     }
 
     class TrackHeaderInfo
     {
-        public void WriteTrackHeader(int trackDataBytes, BinaryWriter a)
+        public void WriteTrackHeader(int partDataBytes, BinaryWriter a)
         {
-            BinaryWriterBE bw = new BinaryWriterBE(a);
+            MidiWriter mw = new MidiWriter(a);
 
-            bw.WriteByte((byte)'M');
-            bw.WriteByte((byte)'T');
-            bw.WriteByte((byte)'r');
-            bw.WriteByte((byte)'k');
+            mw.WriteByte((byte)'M');
+            mw.WriteByte((byte)'T');
+            mw.WriteByte((byte)'r');
+            mw.WriteByte((byte)'k');
 
-            bw.WriteBE4(trackDataBytes);
+            // trackData = partData + trackFooter(4 bytes)
+            mw.WriteBE4(partDataBytes + 4);
         }
 
         public void WriteTrackFooter(BinaryWriter a)
         {
-            BinaryWriterBE bw = new BinaryWriterBE(a);
-            bw.WriteBE4(0x00ff2f00); 
+            MidiWriter mw = new MidiWriter(a);
+            mw.WriteBE4(0x00ff2f00); 
         }
     }
 
@@ -159,8 +280,9 @@ namespace Compose
         private Part CreateMusic()
         {
             Part p = new Part(PartType.Soprano);
-            p.AddNote(new Note(0, 120, NoteType.C, 4, 60));
-            p.AddNote(new Note(127, 120, NoteType.E, 4, 60));
+            p.AddNote(new Note(0, 120, NoteType.C, 4, 0x60));
+            p.AddNote(new Note(128, 120, NoteType.E, 4, 0x60));
+            p.AddNote(new Note(256, 120, NoteType.G, 4, 0x60));
             return p;
         }
 
@@ -169,7 +291,8 @@ namespace Compose
             mhi.WriteMidiHeader(bw);
 
             TrackHeaderInfo thi = new TrackHeaderInfo();
-            thi.WriteTrackHeader(4, bw);
+            thi.WriteTrackHeader(p.CountMidiBytes(), bw);
+            p.WriteMidiPart(bw);
             thi.WriteTrackFooter(bw);
         }
     }
