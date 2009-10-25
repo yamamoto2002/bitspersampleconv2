882381e05014d23a4351c4d660fe8385d366cd0c
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2009-10-25 04:15:28 +0000

    harmonygen2

---
+++ trunk/HarmonyGen2/HarmonyGen2/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HarmonyGen2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Yamamoto DIY")]
+[assembly: AssemblyProduct("HarmonyGen2")]
+[assembly: AssemblyCopyright("Copyright c Yamamoto DIY 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("001b5158-6289-4ec9-b689-1b7fb4424d4b")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
