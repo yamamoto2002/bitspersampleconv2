22d0fdc6409500ef5fc9c7b82f218eb3e686b663
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2010-07-24 04:50:38 +0000

    dprintf macro

--- WasapiIODLL/WWUtil.h
+++ WasapiIODLL/WWUtil.h
@@ -1,31 +1,37 @@
 #pragma once
 
+#ifdef _DEBUG
+#  include <stdio.h>
+#  define dprintf(x, ...) printf(__VA_ARGS__)
+#else
+#  define dprintf(x, ...)
+#endif
 
-#define HRG(x)                                   \
-{                                                \
-    hr = x;                                      \
-    if (FAILED(hr)) {                            \
-        printf("E: %s failed (%08x)\n", #x, hr); \
-        goto end;                                \
-    }                                            \
-}                                                \
+#define HRG(x)                                    \
+{                                                 \
+    hr = x;                                       \
+    if (FAILED(hr)) {                             \
+        dprintf("E: %s failed (%08x)\n", #x, hr); \
+        goto end;                                 \
+    }                                             \
+}                                                 \
 
-#define HRR(x)                                   \
-{                                                \
-    hr = x;                                      \
-    if (FAILED(hr)) {                            \
-        printf("E: %s failed (%08x)\n", #x, hr); \
-        return hr;                               \
-    }                                            \
-}                                                \
+#define HRR(x)                                    \
+{                                                 \
+    hr = x;                                       \
+    if (FAILED(hr)) {                             \
+        dprintf("E: %s failed (%08x)\n", #x, hr); \
+        return hr;                                \
+    }                                             \
+}                                                 \
 
-#define CHK(x)                     \
-{   if (!x) {                      \
-    printf("E: %s is NULL\n", #x); \
-        assert(0);                 \
-        return E_FAIL;             \
-    }                              \
-}                                  \
+#define CHK(x)                          \
+{   if (!x) {                           \
+        dprintf("E: %s is NULL\n", #x); \
+        assert(0);                      \
+        return E_FAIL;                  \
+    }                                   \
+}                                       \
 
 template <class T> void SafeRelease(T **ppT)
 {
