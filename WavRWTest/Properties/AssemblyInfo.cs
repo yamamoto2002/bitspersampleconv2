33cef259a5d154a9a43156731dd6724d6ded8efb
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2010-09-11 06:49:50 +0000

    add WavRWTest project

---
+++ WavRWTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WavRWTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("yamamoto2002")]
+[assembly: AssemblyProduct("WavRWTest")]
+[assembly: AssemblyCopyright("Copyright © yamamoto2002 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("46bbaa6d-4161-411f-ac3c-e53101f91cec")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
