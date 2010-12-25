5c506e3a9e6d83dad1cb67ca90a207e47e2b1b27
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2010-12-25 09:02:26 +0000

    WWDirectComputeCS

---
+++ trunk/WWDirectComputeCS/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WWDirectComputeCS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("yamamoto2002")]
+[assembly: AssemblyProduct("WWDirectComputeCS")]
+[assembly: AssemblyCopyright("yamamoto2002")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0f272ed-5a55-48a6-84a6-ab455fc061a1")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
