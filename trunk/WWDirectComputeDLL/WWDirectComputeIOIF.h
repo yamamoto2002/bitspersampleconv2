cfda91db2530f8aa7063e9ede1560d4769c1a56e
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2010-12-27 18:47:22 +0000

    Upsample

--- trunk/WWDirectComputeDLL/WWDirectComputeIOIF.h
+++ trunk/WWDirectComputeDLL/WWDirectComputeIOIF.h
@@ -2,6 +2,41 @@
 
 #include <Windows.h>
 
+/////////////////////////////////////////////////////////////////////////////
+// アップサンプル
+
+/// @result HRESULT
+extern "C" __declspec(dllexport)
+int __stdcall
+WWDCUpsample_Init(
+        int convolutionN,
+        float * sampleFrom,
+        int sampleTotalFrom,
+        int sampleRateFrom,
+        int sampleRateTo,
+        int sampleTotalTo);
+
+/// @result HRESULT
+extern "C" __declspec(dllexport)
+int __stdcall
+WWDCUpsample_Dispatch(
+        int startPos,
+        int count);
+
+/// @result HRESULT
+extern "C" __declspec(dllexport)
+int __stdcall
+WWDCUpsample_GetResultFromGpuMemory(
+        float * outputTo,
+        int outputToElemNum);
+
+extern "C" __declspec(dllexport)
+void __stdcall
+WWDCUpsample_Term(void);
+
+/////////////////////////////////////////////////////////////////////////////
+// 古い
+
 /// @result HRESULT
 extern "C" __declspec(dllexport)
 int __stdcall
