f6486cf1f25332e8b74094626bf984fd0f895a76
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2010-12-29 19:05:53 +0000

    PlayPcmWin 2.0.51

--- trunk/WWDirectComputeDLL/WWDirectComputeIOIF.h
+++ trunk/WWDirectComputeDLL/WWDirectComputeIOIF.h
@@ -3,7 +3,7 @@
 #include <Windows.h>
 
 /////////////////////////////////////////////////////////////////////////////
-// アップサンプル
+// アップサンプル GPU処理
 
 /// @result HRESULT
 extern "C" __declspec(dllexport)
@@ -83,51 +83,3 @@ extern "C" __declspec(dllexport)
 void __stdcall
 WWDCUpsample_UpsampleCpuUnsetup(void);
 
-/*
-/// @result サンプルデータのスケーリング(0.5=0.5倍スケール)
-extern "C" __declspec(dllexport)
-float __stdcall
-WWDCUpsample_LimitSampleData(
-        float * sampleInOut,
-        int sampleElemNum);
-*/
-
-/////////////////////////////////////////////////////////////////////////////
-// 古い
-
-/// @result HRESULT
-extern "C" __declspec(dllexport)
-int __stdcall
-WWDCIO_Init(
-        int precision,
-        int convolutionN);
-
-extern "C" __declspec(dllexport)
-void __stdcall
-WWDCIO_Term(void);
-
-/// @param precision 0: singleprec 1: doubleprec
-/// @param convolutionN must be a power of 2 and larger than 256
-/// @result HRESULT
-extern "C" __declspec(dllexport)
-int __stdcall
-WWDCIO_JitterAddGpu(
-        int precision,
-        int sampleTotal,
-        int convolutionN,
-        float *sampleData,
-        float *jitterX,
-        float *outF);
-
-/// convolute the specified portion (offs to offs+sampleToProcess-1)
-extern "C" __declspec(dllexport)
-int __stdcall
-WWDCIO_JitterAddGpuPortion(
-        int precision,
-        int sampleTotal,
-        int convolutionN,
-        float *sampleData,
-        float *jitterX,
-        float *outF,
-        int offs,
-        int sampleToProcess);
