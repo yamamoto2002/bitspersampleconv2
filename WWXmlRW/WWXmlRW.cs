ba0f7ae275d0d8de0687bd92bfc1b28123619278
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2011-05-29 12:27:22 +0000

    WW XmlRW

---
+++ WWXmlRW/WWXmlRW.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.IsolatedStorage;
+using System.Xml.Serialization;
+
+namespace WWXmlRW {
+    public interface SaveLoadContents {
+        int GetVersion();
+        int GetCurrentVersion();
+
+    }
+
+    /// <summary>
+    /// TをXML形式でセーブロードする
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class XmlRW<T> where T : class, SaveLoadContents, new() {
+
+        private readonly string m_fileName;
+
+        public XmlRW(string fileName) {
+            m_fileName = fileName;
+        }
+
+        public T Load() {
+            T p = new T();
+
+            try {
+                using (IsolatedStorageFileStream isfs = new IsolatedStorageFileStream(
+                        m_fileName, System.IO.FileMode.Open,
+                        IsolatedStorageFile.GetUserStoreForDomain())) {
+                    byte[] buffer = new byte[isfs.Length];
+                    isfs.Read(buffer, 0, (int)isfs.Length);
+                    System.IO.MemoryStream stream = new System.IO.MemoryStream(buffer);
+                    XmlSerializer formatter = new XmlSerializer(typeof(T));
+                    p = formatter.Deserialize(stream) as T;
+                    isfs.Close();
+                }
+            } catch (System.Exception ex) {
+                Console.WriteLine(ex);
+                p = new T();
+            }
+
+            if (p.GetCurrentVersion() != p.GetVersion()) {
+                Console.WriteLine("Version mismatch {0} != {1}", p.GetCurrentVersion(), p.GetVersion());
+                p = new T();
+            }
+
+            return p;
+        }
+
+        public bool Save(T p) {
+            bool result = false;
+
+            try {
+                using (IsolatedStorageFileStream isfs = new IsolatedStorageFileStream(
+                        m_fileName, System.IO.FileMode.Create,
+                        IsolatedStorageFile.GetUserStoreForDomain())) {
+                    XmlSerializer s = new XmlSerializer(typeof(T));
+                    s.Serialize(isfs, p);
+                    result = true;
+                }
+            } catch (System.Exception ex) {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return result;
+        }
+    }
+}
