4e48ab0d696cb6dda2538ca857918431ad139356
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2011-05-29 15:25:12 +0000

    PlayPcmWin 2.0.96

--- trunk/WWXmlRW/WWXmlRW.cs
+++ trunk/WWXmlRW/WWXmlRW.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.IO.IsolatedStorage;
 using System.Xml.Serialization;
 
@@ -19,24 +20,46 @@ namespace WWXmlRW {
     public class XmlRW<T> where T : class, SaveLoadContents, new() {
 
         private readonly string m_fileName;
+        private bool m_useIsolatedStorage;
 
-        public XmlRW(string fileName) {
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="fileName">useIsolatedStorage==falseの場合、フルパスでファイルを指定。useIsolatedStorage==trueの場合、フォルダ名を含まないファイル名。</param>
+        /// <param name="useIsolatedStorage">true: IsolatedStorageに保存する。false:通常のフォルダに保存。</param>
+        public XmlRW(string fileName, bool useIsolatedStorage = true) {
             m_fileName = fileName;
+            m_useIsolatedStorage = useIsolatedStorage;
         }
 
-        public T Load() {
+        public bool Load(out T t) {
+            bool result = false;
             T p = new T();
 
             try {
-                using (IsolatedStorageFileStream isfs = new IsolatedStorageFileStream(
-                        m_fileName, System.IO.FileMode.Open,
-                        IsolatedStorageFile.GetUserStoreForDomain())) {
-                    byte[] buffer = new byte[isfs.Length];
-                    isfs.Read(buffer, 0, (int)isfs.Length);
-                    System.IO.MemoryStream stream = new System.IO.MemoryStream(buffer);
-                    XmlSerializer formatter = new XmlSerializer(typeof(T));
-                    p = formatter.Deserialize(stream) as T;
-                    isfs.Close();
+                if (m_useIsolatedStorage) {
+                    using (IsolatedStorageFileStream isfs = new IsolatedStorageFileStream(
+                            m_fileName, System.IO.FileMode.Open,
+                            IsolatedStorageFile.GetUserStoreForDomain())) {
+                        byte[] buffer = new byte[isfs.Length];
+                        isfs.Read(buffer, 0, (int)isfs.Length);
+                        System.IO.MemoryStream stream = new System.IO.MemoryStream(buffer);
+                        XmlSerializer formatter = new XmlSerializer(typeof(T));
+                        p = formatter.Deserialize(stream) as T;
+                        isfs.Close();
+                        result = true;
+                    }
+                } else {
+                    using (Stream fs = new FileStream(
+                            m_fileName, System.IO.FileMode.Open)) {
+                        byte[] buffer = new byte[fs.Length];
+                        fs.Read(buffer, 0, (int)fs.Length);
+                        System.IO.MemoryStream stream = new System.IO.MemoryStream(buffer);
+                        XmlSerializer formatter = new XmlSerializer(typeof(T));
+                        p = formatter.Deserialize(stream) as T;
+                        fs.Close();
+                        result = true;
+                    }
                 }
             } catch (System.Exception ex) {
                 Console.WriteLine(ex);
@@ -46,21 +69,38 @@ namespace WWXmlRW {
             if (p.GetCurrentVersion() != p.GetVersion()) {
                 Console.WriteLine("Version mismatch {0} != {1}", p.GetCurrentVersion(), p.GetVersion());
                 p = new T();
+                result = false;
             }
 
-            return p;
+            t = p;
+            return result;
+        }
+
+        public T Load() {
+            T t;
+            Load(out t);
+            return t;
         }
 
         public bool Save(T p) {
             bool result = false;
 
             try {
-                using (IsolatedStorageFileStream isfs = new IsolatedStorageFileStream(
-                        m_fileName, System.IO.FileMode.Create,
-                        IsolatedStorageFile.GetUserStoreForDomain())) {
-                    XmlSerializer s = new XmlSerializer(typeof(T));
-                    s.Serialize(isfs, p);
-                    result = true;
+                if (m_useIsolatedStorage) {
+                    using (IsolatedStorageFileStream isfs = new IsolatedStorageFileStream(
+                            m_fileName, System.IO.FileMode.Create,
+                            IsolatedStorageFile.GetUserStoreForDomain())) {
+                        XmlSerializer s = new XmlSerializer(typeof(T));
+                        s.Serialize(isfs, p);
+                        result = true;
+                    }
+                } else {
+                    using (FileStream fs = new FileStream(
+                            m_fileName, System.IO.FileMode.Create)) {
+                        XmlSerializer s = new XmlSerializer(typeof(T));
+                        s.Serialize(fs, p);
+                        result = true;
+                    }
                 }
             } catch (System.Exception ex) {
                 Console.WriteLine(ex.ToString());
