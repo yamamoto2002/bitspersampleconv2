3d249e9dd2e37fbc4662bbd4add47ba60d28ac6b
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2012-03-31 05:09:33 +0000

    PlayPcmWin 3.0.60

--- trunk/PlayPcmWin/LocBamlExtractx64.bat
+++ trunk/PlayPcmWin/LocBamlExtractx64.bat
@@ -1,6 +1,6 @@
 cd bin\x64\Release
 copy ..\..\..\locbaml_x64.exe locbaml.exe
-#locbaml /parse en-US\PlayPcmWin.resources.dll /out:..\..\..\PlayPcmWinx64.en-US.csv
+rem locbaml /parse en-US\PlayPcmWin.resources.dll /out:..\..\..\PlayPcmWinx64.en-US.csv
 locbaml /parse en-US\PlayPcmWin.resources.dll /out:..\..\..\PlayPcmWin.en-US.csv
 del locbaml.exe
 cd ..\..\..
