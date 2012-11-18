f566161e726f1d8adbb2ea90e127a4eeb30e762a
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2012-11-18 06:30:19 +0000

    PlayPcmWin 4.0.20

--- trunk/PlayPcmWinTestBench/Properties/AssemblyInfo.cs
+++ trunk/PlayPcmWinTestBench/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.53.0")]
-[assembly: AssemblyFileVersion("1.0.53.0")]
+[assembly: AssemblyVersion("1.0.54.0")]
+[assembly: AssemblyFileVersion("1.0.54.0")]
