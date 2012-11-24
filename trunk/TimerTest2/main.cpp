aabdea27607e16d116e2b7f14894e91a3986047c
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2012-11-24 23:05:27 +0000

    NtSetTimerResolution test

---
+++ trunk/TimerTest2/main.cpp
@@ -0,0 +1,41 @@
+#include <Windows.h>
+#include <stdio.h>
+
+/* ntdll.dll is included in Windows Driver Development Kit.
+ * You may need to update VS user library path to:
+ * $(LibraryPath);C:\WinDDK\7600.16385.1\lib\win7\i386
+ * or
+ * $(LibraryPath);C:\WinDDK\7600.16385.1\lib\win7\amd64
+ */
+#pragma comment(lib, "ntdll")
+
+extern "C" {
+extern NTSYSAPI NTSTATUS NTAPI
+NtSetTimerResolution(
+        IN  ULONG   desiredResolution,
+        IN  BOOLEAN setResolution,
+        OUT PULONG  currentResolution);
+
+extern NTSYSAPI NTSTATUS NTAPI
+NtQueryTimerResolution(
+        OUT PULONG minimumResolution,
+        OUT PULONG maximumResolution,
+        OUT PULONG currentResolution);
+};
+
+int main(void)
+{
+    ULONG minResolution = 0U;
+    ULONG maxResolution = 0U;
+    ULONG curResolution = 0U;
+
+    NtQueryTimerResolution(&minResolution, &maxResolution, &curResolution);
+
+    printf("NtQueryTimerResolution min=%u max=%u cur=%u\n",
+            minResolution, maxResolution, curResolution);
+
+    NtSetTimerResolution(maxResolution, TRUE, &curResolution);
+
+    printf("NtSetTimerResolution %u cur=%u\n",
+            maxResolution, curResolution);
+}
