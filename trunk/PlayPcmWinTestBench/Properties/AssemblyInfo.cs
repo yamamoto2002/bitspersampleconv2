c34757e5ececbf6d91bad32cbb273e25bd7f6337
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2013-01-26 05:50:38 +0000

    PlayPcmWin 4.0.35 fix DoP playback glitch

--- trunk/PlayPcmWinTestBench/Properties/AssemblyInfo.cs
+++ trunk/PlayPcmWinTestBench/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.62.0")]
-[assembly: AssemblyFileVersion("1.0.62.0")]
+[assembly: AssemblyVersion("1.0.63.0")]
+[assembly: AssemblyFileVersion("1.0.63.0")]
