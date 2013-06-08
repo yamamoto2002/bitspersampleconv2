340e63ebe451a02446e3ae31919ac0669441362f
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2013-06-08 19:08:42 +0000

    Radix2FFT test

---
+++ trunk/FftTest/WWComplex.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FftTest {
+    struct WWComplex {
+        public double real;
+        public double imaginary;
+
+        public WWComplex(double real, double imaginary) {
+            this.real      = real;
+            this.imaginary = imaginary;
+        }
+
+        public WWComplex(WWComplex rhs) {
+            this.real      = rhs.real;
+            this.imaginary = rhs.imaginary;
+        }
+
+        public double Magnitude() {
+            return Math.Sqrt(real * real + imaginary * imaginary);
+        }
+
+        /// <summary>
+        /// Phase in radians
+        /// </summary>
+        /// <returns>radians, -π to +π</returns>
+        public double Phase() {
+            return Math.Atan2(imaginary, real);
+        }
+
+        public WWComplex Add(WWComplex rhs) {
+            real      += rhs.real;
+            imaginary += rhs.imaginary;
+            return this;
+        }
+
+        public WWComplex Mul(double v) {
+            real      *= v;
+            imaginary *= v;
+            return this;
+        }
+
+        public WWComplex Mul(WWComplex rhs) {
+            double tR = real * rhs.real      - imaginary * rhs.imaginary;
+            double tI = real * rhs.imaginary + imaginary * rhs.real;
+            real      = tR;
+            imaginary = tI;
+            return this;
+        }
+
+        public void CopyFrom(WWComplex rhs) {
+            real      = rhs.real;
+            imaginary = rhs.imaginary;
+        }
+    }
+}
