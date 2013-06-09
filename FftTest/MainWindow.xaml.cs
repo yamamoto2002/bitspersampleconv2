ca4fa2afa8e75c27a77f02b458875eb8de36a0c4
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2013-06-09 05:52:26 +0000

    Radix2FFT upsample test

--- FftTest/MainWindow.xaml.cs
+++ FftTest/MainWindow.xaml.cs
@@ -504,7 +504,7 @@ namespace FftTest {
 
             int upsampledSampleCount = SampleCount() * UpsampleMultiple();
 
-            // 周波数ドメインのFFTデータを0で水増しして逆FFTする
+            // 周波数ドメインデータfreqDomainOrigを0で水増ししたデータfreqDomainUpsampledを作って逆FFTする
             var freqDomainUpsampled = new WWComplex[upsampledSampleCount];
             for (int i=0; i < freqDomainUpsampled.Length; ++i) {
                 if (i <= freqDomainOrig.Length / 2) {
