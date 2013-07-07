2b61bb61a63f727803054e35356ba8b70664c56e
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2013-07-07 11:44:31 +0000

    WWAudioFilter 1.0.3

--- trunk/WWAudioFilter/MashFilter.cs
+++ trunk/WWAudioFilter/MashFilter.cs
@@ -10,6 +10,7 @@ namespace WWAudioFilter {
     class MashFilter : FilterBase {
         public int TargetBitsPerSample { get; set; }
         private NoiseShaperMash mNoiseShaper;
+        private NoiseShaperMash1bit mNoiseShaper1bit;
 
         public MashFilter(int targetBitsPerSample)
                 : base(FilterType.Mash2) {
@@ -49,7 +50,11 @@ namespace WWAudioFilter {
         }
 
         public override void FilterStart() {
-            mNoiseShaper = new NoiseShaperMash(TargetBitsPerSample);
+            if (1 == TargetBitsPerSample) {
+                mNoiseShaper1bit = new NoiseShaperMash1bit();
+            } else {
+                mNoiseShaper = new NoiseShaperMash(TargetBitsPerSample);
+            }
         }
 
         public override void FilterEnd() {
@@ -58,35 +63,55 @@ namespace WWAudioFilter {
 
         public override double[] FilterDo(double[] inPcm) {
             double [] outPcm = new double[inPcm.LongLength];
-            
-            for (long i=0; i < outPcm.LongLength; ++i) {
-                double sampleD = inPcm[i];
 
-                int sampleI24;
-                if (1.0 <= sampleD) {
-                    sampleI24 = 8388607;
-                } else if (sampleD < -1.0) {
-                    sampleI24 = -8388608;
-                } else {
-                    sampleI24 = (int)(8388608 * sampleD);
+            if (1 == TargetBitsPerSample) {
+                for (long i=0; i < outPcm.LongLength; ++i) {
+                    double sampleD = inPcm[i];
+
+                    int sampleI24;
+                    if (1.0 <= sampleD) {
+                        sampleI24 = 8388607;
+                    } else if (sampleD < -1.0) {
+                        sampleI24 = -8388608;
+                    } else {
+                        sampleI24 = (int)(8388608 * sampleD);
+                    }
+
+                    sampleI24 = mNoiseShaper1bit.Filter24(sampleI24);
+
+                    outPcm[i] = (double)sampleI24 * (1.0 / 8388608);
                 }
+            } else {
+                for (long i=0; i < outPcm.LongLength; ++i) {
+                    double sampleD = inPcm[i];
 
-                sampleI24 = mNoiseShaper.Filter24(sampleI24);
+                    int sampleI24;
+                    if (1.0 <= sampleD) {
+                        sampleI24 = 8388607;
+                    } else if (sampleD < -1.0) {
+                        sampleI24 = -8388608;
+                    } else {
+                        sampleI24 = (int)(8388608 * sampleD);
+                    }
 
-                outPcm[i] = (double)sampleI24 * (1.0 / 8388608);
+                    sampleI24 = mNoiseShaper.Filter24(sampleI24);
+
+                    outPcm[i] = (double)sampleI24 * (1.0 / 8388608);
+                }
             }
 
             return outPcm;
         }
 
         /////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // 2 or more bit version
 
         class SigmaDelta {
-            protected int mQuantizedBit;
-            protected uint mMask;
-            protected double mDelayX;
-            protected double mDelayY;
-            protected double mQuantizationError;
+            private int mQuantizedBit;
+            private uint mMask;
+            private double mDelayX;
+            private double mDelayY;
+            private double mQuantizationError;
 
             /// <summary>
             /// SigmaDelta noise shaping system
@@ -110,7 +135,7 @@ namespace WWAudioFilter {
             /// </summary>
             /// <param name="sampleFrom">input data. 24bit signed (-2^23 to +2^23-1)</param>
             /// <returns>filtered value. 24bit signed</returns>
-            public virtual int Filter24(double sampleFrom) {
+            public int Filter24(double sampleFrom) {
                 // convert quantized bit rate to 32bit integer
                 sampleFrom *= 256;
 
@@ -134,36 +159,6 @@ namespace WWAudioFilter {
             }
         }
 
-        class SigmaDelta1bit : SigmaDelta {
-            /// <summary>
-            /// SigmaDelta 1bit noise shaping system
-            /// </summary>
-            public SigmaDelta1bit() : base(1) {
-            }
-
-            /// <summary>
-            /// input sampleFrom, returns quantized sample value
-            /// </summary>
-            /// <param name="sampleFrom">input data. 24bit signed (-2^23 to +2^23-1)</param>
-            /// <returns>filtered value. 24bit signed</returns>
-            public override int Filter24(double sampleFrom) {
-                // convert quantized bit rate to 32bit integer
-                sampleFrom *= 256;
-
-                double x = sampleFrom + mDelayX - mDelayY;
-                mDelayX = x;
-
-                double y1q = x;
-
-                int sampleY = (0 <= y1q) ? Int32.MaxValue : Int32.MinValue;
-                mDelayY = sampleY;
-
-                mQuantizationError = (sampleY - x) / 256;
-
-                return sampleY / 256;
-            }
-        }
-
         class NoiseShaperMash {
             private SigmaDelta [] mSds;
             private double mDelayY1;
@@ -172,14 +167,8 @@ namespace WWAudioFilter {
             public NoiseShaperMash(int quantizedBit) {
                 mSds = new SigmaDelta[2];
 
-                if (quantizedBit == 1) {
-                    for (int i=0; i < 2; ++i) {
-                        mSds[i] = new SigmaDelta1bit();
-                    }
-                } else {
-                    for (int i=0; i < 2; ++i) {
-                        mSds[i] = new SigmaDelta(quantizedBit);
-                    }
+                for (int i=0; i < 2; ++i) {
+                    mSds[i] = new SigmaDelta(quantizedBit);
                 }
             }
 
@@ -203,6 +192,73 @@ namespace WWAudioFilter {
 
                 r /= 256;
                 return (int)r;
+            }
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+        // 1bit version
+
+        class SigmaDelta1bit {
+            private double mDelayX;
+            private double mDelayY;
+            private double mQuantizationError;
+
+            /// <summary>
+            /// SigmaDelta 1bit noise shaping system
+            /// </summary>
+            public SigmaDelta1bit() {
+            }
+
+            public double QuantizationError() {
+                return mQuantizationError;
+            }
+
+            /// <summary>
+            /// input sampleFrom, returns quantized sample value
+            /// </summary>
+            /// <param name="sampleFrom">input data. 24bit signed (-2^23 to +2^23-1)</param>
+            /// <returns>filtered value. 24bit signed</returns>
+            public int Filter24(double sampleFrom) {
+                // convert quantized bit rate to 32bit integer
+                sampleFrom *= 256;
+
+                double x = sampleFrom + mDelayX - mDelayY;
+                mDelayX = x;
+
+                double y1q = x;
+
+                int sampleY = (0 <= y1q) ? Int32.MaxValue : Int32.MinValue;
+                mDelayY = sampleY;
+
+                mQuantizationError = (sampleY - x) / 256;
+
+                return sampleY / 256;
+            }
+        }
+
+        class NoiseShaperMash1bit {
+            private SigmaDelta1bit [] mSds;
+            private double mDelayY1;
+            private double mDelayY2;
+
+            public NoiseShaperMash1bit() {
+                mSds = new SigmaDelta1bit[2];
+
+                for (int i=0; i < 2; ++i) {
+                    mSds[i] = new SigmaDelta1bit();
+                }
+            }
+
+            public int Filter24(int sampleFrom) {
+                double y1 = mSds[0].Filter24(sampleFrom);
+                double y2 = mSds[1].Filter24(mSds[0].QuantizationError());
+
+                double r = mDelayY1 - (y2 - mDelayY2);
+
+                mDelayY1 = y1;
+                mDelayY2 = y2;
+
+                return (0 <= r) ? 8388607 : -8388608;
             }
         }
     }
