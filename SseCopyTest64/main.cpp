e4ea1903537529bcf6fb60c04d6c5cc3a39e6f9e
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2013-07-15 11:21:51 +0000

    x64 masm test

--- SseCopyTest64/main.cpp
+++ SseCopyTest64/main.cpp
@@ -19,47 +19,47 @@ int main(void)
     LARGE_INTEGER after;
 
     for (int j=0; j<10; ++j) {
-    // test memcpy performance
-    memset(from, 0x69, BUFFER_SIZE);
-    QueryPerformanceCounter(&before);
-    for (int i=0; i<100; ++i) {
-        memcpy(to, from, BUFFER_SIZE);
-    }
-    QueryPerformanceCounter(&after);
-    printf("memcpy %f\n", (after.QuadPart - before.QuadPart) * 1000.0 * 1000 / freq.QuadPart);
-
-    // test MyMemcpy64 performance
-    for (int i=0; i<BUFFER_SIZE; ++i) {
-        from[i] = (char)i;
-    }
-    QueryPerformanceCounter(&before);
-    for (int i=0; i<100; ++i) {
-        MyMemcpy64(to, from, BUFFER_SIZE);
-    }
-    QueryPerformanceCounter(&after);
-    printf("MyMemcpy64 %f\n", (after.QuadPart - before.QuadPart) * 1000.0 * 1000 / freq.QuadPart);
-
-    for (int i=0; i<BUFFER_SIZE; ++i) {
-        if (to[i] != from[i]) {
-            printf("to[%d](%x) != from[%d](%x)\n", i, (int)to[i], i, (int)from[i]);
+        // test memcpy performance
+        memset(from, 0x69, BUFFER_SIZE);
+        QueryPerformanceCounter(&before);
+        for (int i=0; i<100; ++i) {
+            memcpy(to, from, BUFFER_SIZE);
         }
-    }
+        QueryPerformanceCounter(&after);
+        printf("memcpy %f\n", (after.QuadPart - before.QuadPart) * 1000.0 * 1000 / freq.QuadPart);
 
-    // test MyMemcpy64a performance
-    for (int i=0; i<BUFFER_SIZE; ++i) {
-        from[i] = (char)(i+69);
-    }
-    QueryPerformanceCounter(&before);
-    for (int i=0; i<100; ++i) {
-        MyMemcpy64a(to, from, BUFFER_SIZE);
-    }
-    QueryPerformanceCounter(&after);
-    printf("MyMemcpy64a %f\n", (after.QuadPart - before.QuadPart) * 1000.0 * 1000 / freq.QuadPart);
-    for (int i=0; i<BUFFER_SIZE; ++i) {
-        if (to[i] != from[i]) {
-            printf("to[%d](%x) != from[%d](%x)\n", i, (int)to[i], i, (int)from[i]);
+        // test MyMemcpy64 performance
+        for (int i=0; i<BUFFER_SIZE; ++i) {
+            from[i] = (char)i;
         }
-    }
+        QueryPerformanceCounter(&before);
+        for (int i=0; i<100; ++i) {
+            MyMemcpy64(to, from, BUFFER_SIZE);
+        }
+        QueryPerformanceCounter(&after);
+        printf("MyMemcpy64 %f\n", (after.QuadPart - before.QuadPart) * 1000.0 * 1000 / freq.QuadPart);
+
+        for (int i=0; i<BUFFER_SIZE; ++i) {
+            if (to[i] != from[i]) {
+                printf("to[%d](%x) != from[%d](%x)\n", i, (int)to[i], i, (int)from[i]);
+            }
+        }
+
+        // test MyMemcpy64a performance
+        for (int i=0; i<BUFFER_SIZE; ++i) {
+            from[i] = (char)(i+69);
+        }
+        QueryPerformanceCounter(&before);
+        for (int i=0; i<100; ++i) {
+            MyMemcpy64a(to, from, BUFFER_SIZE);
+        }
+        QueryPerformanceCounter(&after);
+        printf("MyMemcpy64a %f\n", (after.QuadPart - before.QuadPart) * 1000.0 * 1000 / freq.QuadPart);
+        for (int i=0; i<BUFFER_SIZE; ++i) {
+            if (to[i] != from[i]) {
+                printf("to[%d](%x) != from[%d](%x)\n", i, (int)to[i], i, (int)from[i]);
+            }
+        }
     }
 
     _aligned_free(to);
