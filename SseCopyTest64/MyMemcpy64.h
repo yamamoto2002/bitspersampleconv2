008a553cbeec61207108f894b1ce48681684a925
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2013-07-15 07:56:28 +0000

    x64 masm test

---
+++ SseCopyTest64/MyMemcpy64.h
@@ -0,0 +1,7 @@
+#pragma once
+
+/// @param dst must be aligned by 16 bytes
+/// @param src must be aligned by 16 bytes
+/// @param bytes must be multiply of 128
+extern "C" void MyMemcpy64(char *dst, const char *src, int bytes);
+
