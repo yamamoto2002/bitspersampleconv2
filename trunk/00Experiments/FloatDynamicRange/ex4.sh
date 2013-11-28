2e3cd625a5b5196e0c228e7b1d26f54b6cbf8c7b
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2013-11-28 13:06:13 +0000

    FloatDynamicRange project

---
+++ trunk/00Experiments/FloatDynamicRange/ex4.sh
@@ -0,0 +1,110 @@
+#!/bin/bash -x
+
+./FloatDynamicRange.exe -generate32 original.bin
+./FloatDynamicRange.exe -convert32 original.bin 1 1048576 M120dB.bin
+./FloatDynamicRange.exe -convert32 M120dB.bin  1 1048576 M241dB.bin
+./FloatDynamicRange.exe -convert32 M241dB.bin  1 1048576 M361dB.bin
+./FloatDynamicRange.exe -convert32 M361dB.bin  1 1048576 M482dB.bin
+./FloatDynamicRange.exe -convert32 M482dB.bin  1 1048576 M602dB.bin
+./FloatDynamicRange.exe -convert32 M602dB.bin  1 1048576 M722dB.bin
+./FloatDynamicRange.exe -convert32 M722dB.bin  1 1048576 M843dB.bin
+
+./FloatDynamicRange.exe -convert32 M722dB.bin        1048576 1 M722dBP120dB.bin
+./FloatDynamicRange.exe -convert32 M722dBP120dB.bin  1048576 1 M722dBP241dB.bin
+./FloatDynamicRange.exe -convert32 M722dBP241dB.bin  1048576 1 M722dBP361dB.bin
+./FloatDynamicRange.exe -convert32 M722dBP361dB.bin  1048576 1 M722dBP482dB.bin
+./FloatDynamicRange.exe -convert32 M722dBP482dB.bin  1048576 1 M722dBP602dB.bin
+./FloatDynamicRange.exe -convert32 M722dBP602dB.bin  1048576 1 M722dBP722dB.bin
+
+diff -s original.bin M722dBP722dB.bin
+
+./FloatDynamicRange.exe -convert32 M843dB.bin        1048576 1 M843dBP120dB.bin
+./FloatDynamicRange.exe -convert32 M843dBP120dB.bin  1048576 1 M843dBP241dB.bin
+./FloatDynamicRange.exe -convert32 M843dBP241dB.bin  1048576 1 M843dBP361dB.bin
+./FloatDynamicRange.exe -convert32 M843dBP361dB.bin  1048576 1 M843dBP482dB.bin
+./FloatDynamicRange.exe -convert32 M843dBP482dB.bin  1048576 1 M843dBP602dB.bin
+./FloatDynamicRange.exe -convert32 M843dBP602dB.bin  1048576 1 M843dBP722dB.bin
+./FloatDynamicRange.exe -convert32 M843dBP722dB.bin  1048576 1 M843dBP843dB.bin
+
+diff -s original.bin M843dBP843dB.bin
+
+# -722dBして+722dBすると元に戻る。
+# -843dBして+843dBすると元に戻らない。
+#
+#  0.5倍 == 20 * log10(0.5) = -6.0205999dB
+#
+# 120 * -6.0205999 == 722dB
+# 140 * -6.0205999 == 843dB
+# 130 * -6.0205999 == 783dB
+
+./FloatDynamicRange.exe -convert32 M722dB.bin  1 1024 M783dB.bin
+
+./FloatDynamicRange.exe -convert32 M783dB.bin        1048576 1 M783dBP120dB.bin
+./FloatDynamicRange.exe -convert32 M783dBP120dB.bin  1048576 1 M783dBP241dB.bin
+./FloatDynamicRange.exe -convert32 M783dBP241dB.bin  1048576 1 M783dBP361dB.bin
+./FloatDynamicRange.exe -convert32 M783dBP361dB.bin  1048576 1 M783dBP482dB.bin
+./FloatDynamicRange.exe -convert32 M783dBP482dB.bin  1048576 1 M783dBP602dB.bin
+./FloatDynamicRange.exe -convert32 M783dBP602dB.bin  1048576 1 M783dBP722dB.bin
+./FloatDynamicRange.exe -convert32 M783dBP722dB.bin  1024    1 M783dBP783dB.bin
+
+diff -s original.bin M783dBP783dB.bin
+
+# -722dBして+722dBすると元に戻る。
+# -783dBして+783dBすると元に戻らない。
+
+# 120 * -6.0205999 == 722dB
+# 130 * -6.0205999 == 783dB
+# 125 * -6.0205999 == 753dB
+
+./FloatDynamicRange.exe -convert32 M722dB.bin  1 32 M753dB.bin
+
+./FloatDynamicRange.exe -convert32 M753dB.bin        1048576 1 M753dBP120dB.bin
+./FloatDynamicRange.exe -convert32 M753dBP120dB.bin  1048576 1 M753dBP241dB.bin
+./FloatDynamicRange.exe -convert32 M753dBP241dB.bin  1048576 1 M753dBP361dB.bin
+./FloatDynamicRange.exe -convert32 M753dBP361dB.bin  1048576 1 M753dBP482dB.bin
+./FloatDynamicRange.exe -convert32 M753dBP482dB.bin  1048576 1 M753dBP602dB.bin
+./FloatDynamicRange.exe -convert32 M753dBP602dB.bin  1048576 1 M753dBP722dB.bin
+./FloatDynamicRange.exe -convert32 M753dBP722dB.bin  32      1 M753dBP753dB.bin
+
+diff -s original.bin M753dBP753dB.bin
+
+# -753dBして+753dBすると元に戻る。
+# -783dBして+783dBすると元に戻らない。
+
+# 125 * -6.0205999 == 753dB
+# 130 * -6.0205999 == 783dB
+# 127 * -6.0205999 == 765dB
+
+./FloatDynamicRange.exe -convert32 M722dB.bin  1 128 M765dB.bin
+
+./FloatDynamicRange.exe -convert32 M765dB.bin        1048576 1 M765dBP120dB.bin
+./FloatDynamicRange.exe -convert32 M765dBP120dB.bin  1048576 1 M765dBP241dB.bin
+./FloatDynamicRange.exe -convert32 M765dBP241dB.bin  1048576 1 M765dBP361dB.bin
+./FloatDynamicRange.exe -convert32 M765dBP361dB.bin  1048576 1 M765dBP482dB.bin
+./FloatDynamicRange.exe -convert32 M765dBP482dB.bin  1048576 1 M765dBP602dB.bin
+./FloatDynamicRange.exe -convert32 M765dBP602dB.bin  1048576 1 M765dBP722dB.bin
+./FloatDynamicRange.exe -convert32 M765dBP722dB.bin  128     1 M765dBP765dB.bin
+
+diff -s original.bin M765dBP765dB.bin
+
+# -753dBして+753dBすると元に戻る。
+# -765dBして+765dBすると元に戻らない。
+
+# 125 * -6.0205999 == 753dB
+# 127 * -6.0205999 == 765dB
+# 126 * -6.0205999 == 758dB
+
+./FloatDynamicRange.exe -convert32 M722dB.bin  1 64 M758.bin
+
+./FloatDynamicRange.exe -convert32 M758dB.bin        1048576 1 M758dBP120dB.bin
+./FloatDynamicRange.exe -convert32 M758dBP120dB.bin  1048576 1 M758dBP241dB.bin
+./FloatDynamicRange.exe -convert32 M758dBP241dB.bin  1048576 1 M758dBP361dB.bin
+./FloatDynamicRange.exe -convert32 M758dBP361dB.bin  1048576 1 M758dBP482dB.bin
+./FloatDynamicRange.exe -convert32 M758dBP482dB.bin  1048576 1 M758dBP602dB.bin
+./FloatDynamicRange.exe -convert32 M758dBP602dB.bin  1048576 1 M758dBP722dB.bin
+./FloatDynamicRange.exe -convert32 M758dBP722dB.bin  64      1 M758dBP758dB.bin
+
+diff -s original.bin M758dBP758dB.bin
+
+# -753dBして+753dBすると元に戻る。
+# -758dBして+758dBすると元に戻らない。
