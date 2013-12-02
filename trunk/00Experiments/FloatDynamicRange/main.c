1957f399b2bee5cff0452d1a263d56f9fc892a51
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2013-12-02 11:59:26 +0000

    FloatDynamicRange project

--- trunk/00Experiments/FloatDynamicRange/main.c
+++ trunk/00Experiments/FloatDynamicRange/main.c
@@ -2,13 +2,14 @@
 #include <string.h>
 #include <stdlib.h> //< atoi()
 #include <assert.h>
+#include <xmmintrin.h> //< _MM_SET_FLUSH_ZERO_MODE
 
 static void PrintUsage(const char *programName)
 {
     printf("Usage\n"
         "%s -generate32 bitdepth writeFilePath\n"
         "    Generate file that contains 32bit float values\n"
-        "%s -convert32 bitdepth readFilePath numerator denominator writeFilePath\n"
+        "%s -convert32 [-ftz] bitdepth readFilePath numerator denominator writeFilePath\n"
         "    read float values from readFilePath and scale sample value by numerator/deniminator and write to writeFilePath\n",
         programName, programName);
 }
@@ -134,21 +135,27 @@ static int ReadConvertWrite32(int argc, char *argv[])
     float multiplier;
     int rv = 1;
     int bitDepth = 0;
+    int ftz = 0;
 
     if (0 != strcmp(argv[1], "-convert32")) {
         PrintUsage(argv[0]);
         return 1;
     }
 
-    bitDepth = atoi(argv[2]);
+    if (0 == strcmp(argv[2], "-ftz")) {
+        ftz = 1;
+        _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+    }
+
+    bitDepth = atoi(argv[ftz+2]);
     if (bitDepth <= 0 || 28 < bitDepth) {
         printf("E: bitdepth must be larger than 0 and smaller than 29\n");
         PrintUsage(argv[0]);
         return 1;
     }
 
-    numerator   = atoi(argv[4]);
-    denominator = atoi(argv[5]);
+    numerator   = atoi(argv[ftz+4]);
+    denominator = atoi(argv[ftz+5]);
     if (numerator == 0 || denominator == 0) {
         printf("E: numerator and denominator must not be zero\n");
         return 1;
@@ -156,15 +163,15 @@ static int ReadConvertWrite32(int argc, char *argv[])
 
     multiplier = (float)numerator / denominator;
 
-    ercd = fopen_s(&fpr, argv[3], "rb");
+    ercd = fopen_s(&fpr, argv[ftz+3], "rb");
     if (ercd != 0 || NULL == fpr) {
-        printf("E: file open error %d %s\n", ercd, argv[3]);
+        printf("E: file open error %d %s\n", ercd, argv[ftz+3]);
         return 1;
     }
 
-    ercd = fopen_s(&fpw, argv[6], "wb");
+    ercd = fopen_s(&fpw, argv[ftz+6], "wb");
     if (ercd != 0 || NULL == fpw) {
-        printf("E: file open error %d %s\n", ercd, argv[6]);
+        printf("E: file open error %d %s\n", ercd, argv[ftz+6]);
 
         fclose(fpr);
         fpr = NULL;
@@ -189,6 +196,7 @@ int main(int argc, char *argv[])
         return Generate32(argc, argv);
         break;
     case 7:
+    case 8:
         return ReadConvertWrite32(argc, argv);
     default:
         PrintUsage(argv[0]);
