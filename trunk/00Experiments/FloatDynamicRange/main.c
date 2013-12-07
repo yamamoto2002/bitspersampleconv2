71c4dbe2d0914ff66d87349c2c92e1a443ca59b0
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2013-12-07 14:45:21 +0000

    FloatDynamicRange -add32 option

--- trunk/00Experiments/FloatDynamicRange/main.c
+++ trunk/00Experiments/FloatDynamicRange/main.c
@@ -11,6 +11,8 @@ static void PrintUsage(const char *programName)
         "    Generate file that contains 32bit float values\n"
         "%s -convert32 [-ftz] bitdepth readFilePath numerator denominator writeFilePath\n"
         "    read float values from readFilePath and scale sample value by numerator/deniminator and write to writeFilePath\n",
+        "%s -add32 readFilePath numerator denominator writeFilePath\n"
+        "    read float values from readFilePath and add sample value with numerator/deniminator and write to writeFilePath\n",
         programName, programName);
 }
 
@@ -71,6 +73,8 @@ static int Generate32(int argc, char *argv[])
 
     return rv;
 }
+
+///////////////////////////////////////////////////////////////////////////////////
 
 static int Convert32(FILE *fpr, float multiplier, FILE *fpw)
 {
@@ -134,7 +138,6 @@ static int ReadConvertWrite32(int argc, char *argv[])
     int denominator;
     float multiplier;
     int rv = 1;
-    int bitDepth = 0;
     int ftz = 0;
 
     if (0 != strcmp(argv[1], "-convert32")) {
@@ -145,13 +148,6 @@ static int ReadConvertWrite32(int argc, char *argv[])
     if (0 == strcmp(argv[2], "-ftz")) {
         ftz = 1;
         _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
-    }
-
-    bitDepth = atoi(argv[ftz+2]);
-    if (bitDepth <= 0 || 28 < bitDepth) {
-        printf("E: bitdepth must be larger than 0 and smaller than 29\n");
-        PrintUsage(argv[0]);
-        return 1;
     }
 
     numerator   = atoi(argv[ftz+4]);
@@ -188,6 +184,113 @@ static int ReadConvertWrite32(int argc, char *argv[])
     return rv;
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////
+
+static int Add32(FILE *fpr, float add, FILE *fpw)
+{
+    int i;
+    long fileBytes = 0;
+    long buffCount = 0;
+    long buffBytes = 0;
+    float *buff = NULL;
+    size_t sz = 0;
+
+    assert(fpr);
+    assert(fpw);
+
+    fseek(fpr, 0, SEEK_END);
+    fileBytes = ftell(fpr);
+    fseek(fpr, 0, SEEK_SET);
+    if (fileBytes <= 0) {
+        printf("E: read file size is too small\n");
+        return 1;
+    }
+
+    buffCount = fileBytes/sizeof(float);
+    // buffBytesはファイルサイズを4の倍数に切り捨てた値になる。
+    buffBytes = buffCount * sizeof(float);
+    buff = (float *)malloc(buffBytes);
+    if (buff == NULL) {
+        printf("E: could not allocate memory\n");
+        return 1;
+    }
+
+    sz = fread(buff, 1, buffBytes, fpr);
+    if (sz != buffBytes) {
+        printf("E: fread() failed\n");
+        buff = NULL;
+        return 1;
+    }
+
+    for (i=0; i<buffCount; ++i) {
+        buff[i] += add;
+    }
+
+    sz = fwrite(buff, 1, buffBytes, fpw);
+    if (sz != buffBytes) {
+        printf("E: fwrite() failed\n");
+        buff = NULL;
+        return 1;
+    }
+
+    free(buff);
+    buff = NULL;
+
+    return 0;
+}
+
+static int ReadAddWrite32(int argc, char *argv[])
+{
+    FILE *fpw = NULL;
+    FILE *fpr = NULL;
+    errno_t ercd;
+    int numerator;
+    int denominator;
+    float add;
+    int rv = 1;
+    int ftz = 0;
+
+    if (0 != strcmp(argv[1], "-add32") || argc != 6) {
+        PrintUsage(argv[0]);
+        return 1;
+    }
+
+    numerator   = atoi(argv[3]);
+    denominator = atoi(argv[4]);
+    if (numerator == 0 || denominator == 0) {
+        printf("E: numerator and denominator must not be zero\n");
+        return 1;
+    }
+
+    add = (float)numerator / denominator;
+
+    ercd = fopen_s(&fpr, argv[2], "rb");
+    if (ercd != 0 || NULL == fpr) {
+        printf("E: file open error %d %s\n", ercd, argv[ftz+3]);
+        return 1;
+    }
+
+    ercd = fopen_s(&fpw, argv[5], "wb");
+    if (ercd != 0 || NULL == fpw) {
+        printf("E: file open error %d %s\n", ercd, argv[ftz+6]);
+
+        fclose(fpr);
+        fpr = NULL;
+        return 1;
+    }
+
+    rv = Add32(fpr, add, fpw);
+
+    fclose(fpw);
+    fpw = NULL;
+    fclose(fpr);
+    fpr = NULL;
+
+    return rv;
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+
 int main(int argc, char *argv[])
 {
     int rv = 1;
@@ -195,6 +298,8 @@ int main(int argc, char *argv[])
     case 4:
         return Generate32(argc, argv);
         break;
+    case 6:
+        return ReadAddWrite32(argc, argv);
     case 7:
     case 8:
         return ReadConvertWrite32(argc, argv);
