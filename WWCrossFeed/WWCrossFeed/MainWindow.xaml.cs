dee717e878c2b4493203944b58c3558a4c32d294
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2014-07-05 09:24:06 +0000

    Crossfeed experiment

---
+++ WWCrossFeed/WWCrossFeed/MainWindow.xaml.cs
@@ -0,0 +1,272 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WWCrossFeed {
+    public partial class MainWindow : Window {
+        private const float CAMERA_DISTANCE_DEFAULT = 10.0f;
+        private const double SMALLEST_ROOM_LENGTH = 2.0;
+        private WWRoom mRoom = new WWRoom();
+        private WWRoomVisualizer mRoomVisualizer;
+        private bool mInitialized = false;
+
+        public MainWindow() {
+            InitializeComponent();
+            mInitialized = true;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            mRoomVisualizer = new WWRoomVisualizer(mCanvas);
+            mRoomVisualizer.SetRoom(mRoom);
+            mRoomVisualizer.ResetCamera(CAMERA_DISTANCE_DEFAULT);
+
+            if (!UpdateParameters()) {
+                Close();
+            }
+
+            UpdateRoomCanvas();
+        }
+
+        private bool UpdateParameters() {
+            bool result = true;
+
+            // listener
+            WW3DModel model;
+            model = LoadModel((mRadioButtonGenerateDefaultListener.IsChecked == true) ? "" : mListenerModelPath.Text, Properties.Resources.listenerModel);
+            if (model == null) {
+                return false;
+            }
+            mRoom.ListenerModel = model;
+            if (!SetupListenerPosition()) {
+                return false;
+            }
+
+            // room model
+            if (mRadioButtonCreateRoomFromDimension.IsChecked == true) {
+                mRoom.RoomModel = GenerateRoomModelFromDimension();
+            } else {
+                mRoom.RoomModel = WWWaveFrontObjReader.ReadFromFile(mRoomModelPath.Text);
+                result = (mRoom.RoomModel != null);
+            }
+            if (!result) {
+                return false;
+            }
+
+            // speaker
+            model = LoadModel((mRadioButtonGenerateDefaultSpeaker.IsChecked == true) ? "" : mSpeakerModelPath.Text, Properties.Resources.speakerModel);
+            if (model == null) {
+                return false;
+            }
+            mRoom.SpeakerModel = model;
+            if (!SetupSpeakerPosition()) {
+                return false;
+            }
+
+            // camera
+            if (!SetupCamera()) {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool SetupCamera() {
+            double fovH;
+
+            if (!Double.TryParse(mTextBoxCameraFovH.Text, out fovH)) {
+                MessageBox.Show("Error: Camera parameter parse error");
+                return false;
+            }
+            mRoomVisualizer.CameraFovHDegree = (float)fovH;
+            return true;
+        }
+
+        private bool SetupListenerPosition() {
+            double x, y, z;
+
+            if (!Double.TryParse(mTextBoxListenerPositionX.Text, out x) ||
+                    !Double.TryParse(mTextBoxListenerPositionY.Text, out y) ||
+                    !Double.TryParse(mTextBoxListenerPositionZ.Text, out z)) {
+                MessageBox.Show("Error: Listener position parse error");
+                return false;
+            }
+            mRoom.ListenerPos = new Vector3D(x, y, z);
+            return true;
+        }
+
+        private bool SetupSpeakerPosition() {
+            double x, y, z;
+
+            if (!Double.TryParse(mTextBoxLeftSpeakerX.Text, out x) ||
+                    !Double.TryParse(mTextBoxLeftSpeakerY.Text, out y) ||
+                    !Double.TryParse(mTextBoxLeftSpeakerZ.Text, out z)) {
+                MessageBox.Show("Error: Left speaker position parse error");
+                return false;
+            }
+            mRoom.SetSpeakerPos(0, new Point3D(x, y, z));
+
+            if (!Double.TryParse(mTextBoxRightSpeakerX.Text, out x) ||
+                    !Double.TryParse(mTextBoxRightSpeakerY.Text, out y) ||
+                    !Double.TryParse(mTextBoxRightSpeakerZ.Text, out z)) {
+                MessageBox.Show("Error: Left speaker position parse error");
+                return false;
+            }
+            mRoom.SetSpeakerPos(1, new Point3D(x, y, z));
+
+            if (!Double.TryParse(mTextBoxLeftSpeakerDX.Text, out x) ||
+                    !Double.TryParse(mTextBoxLeftSpeakerDY.Text, out y) ||
+                    !Double.TryParse(mTextBoxLeftSpeakerDZ.Text, out z) ||
+                new Vector3D(x, y, z).LengthSquared < float.Epsilon) {
+                MessageBox.Show("Error: Left speaker direction parse error");
+                return false;
+            }
+            {
+                var dir = new Vector3D(x, y, z);
+                dir.Normalize();
+                mRoom.SetSpeakerDir(0, dir);
+            }
+
+            if (!Double.TryParse(mTextBoxRightSpeakerDX.Text, out x) ||
+                    !Double.TryParse(mTextBoxRightSpeakerDY.Text, out y) ||
+                    !Double.TryParse(mTextBoxRightSpeakerDZ.Text, out z)) {
+                MessageBox.Show("Error: Left speaker direction parse error");
+                return false;
+            }
+            {
+                var dir = new Vector3D(x, y, z);
+                dir.Normalize();
+                mRoom.SetSpeakerDir(1, dir);
+            }
+            return true;
+        }
+
+        private WW3DModel GenerateRoomModelFromDimension() {
+            double w, d, h;
+
+            if (!Double.TryParse(mRoomW.Text, out w) || w < SMALLEST_ROOM_LENGTH ||
+                    !Double.TryParse(mRoomD.Text, out d) || d < SMALLEST_ROOM_LENGTH ||
+                    !Double.TryParse(mRoomH.Text, out h) || h < SMALLEST_ROOM_LENGTH) {
+                MessageBox.Show("Error: Set room size larger than or equal to " + SMALLEST_ROOM_LENGTH);
+                return null;
+            }
+            return WWModeler.GenerateCuboid(new Size3D(w, h, d), new Vector3D(0, h/2, 0), WWModeler.NormalDirection.Inward);
+        }
+
+        private WW3DModel LoadModel(string path, byte[] defaultModel) {
+            WW3DModel model;
+
+            if (path == null || path.Length == 0) {
+                model = WWWaveFrontObjReader.ReadFromStream(new MemoryStream(defaultModel));
+            } else {
+                model = WWWaveFrontObjReader.ReadFromFile(path);
+            }
+
+            if (model == null) {
+                MessageBox.Show("Error: Could not read file: " + path);
+                return null;
+            }
+            return model;
+        }
+
+        private void UpdateRoomCanvas() {
+            mRoomVisualizer.Redraw();
+        }
+
+        private void mRadioButtonCreateRoomFromDimension_Checked(object sender, RoutedEventArgs e) {
+            if (!mInitialized) {
+                return;
+            }
+            mGroupBoxRoomDimension.IsEnabled = true;
+            mGroupBoxRoomFile.IsEnabled = false;
+        }
+
+        private void mRadioButtonCreateRoomFromFile_Checked(object sender, RoutedEventArgs e) {
+            if (!mInitialized) {
+                return;
+            }
+            mGroupBoxRoomDimension.IsEnabled = false;
+            mGroupBoxRoomFile.IsEnabled = true;
+        }
+
+        private void mRadioButtonGenerateDefaultListener_Checked(object sender, RoutedEventArgs e) {
+            if (!mInitialized) {
+                return;
+            }
+            mGroupBoxListenerModelFile.IsEnabled = false;
+        }
+
+        private void mRadioButtonGenerateListenerFromFile_Checked(object sender, RoutedEventArgs e) {
+            if (!mInitialized) {
+                return;
+            }
+            mGroupBoxListenerModelFile.IsEnabled = true;
+        }
+
+        private void ButtonBrowseListenerModel_Click(object sender, RoutedEventArgs e) {
+            var dlg = new OpenFileDialog();
+            dlg.CheckFileExists = true;
+            dlg.CheckPathExists = true;
+            dlg.ValidateNames = true;
+            dlg.Multiselect = false;
+            dlg.DefaultExt = ".obj";
+            dlg.Filter = "Wavefront Obj (.obj)|*.obj";
+            var result = dlg.ShowDialog();
+            if (result != true) {
+                return;
+            }
+
+            var model = WWWaveFrontObjReader.ReadFromFile(dlg.FileName);
+            if (model == null) {
+                return;
+            }
+
+            mListenerModelPath.Text = dlg.FileName;
+
+            mRoom.ListenerModel = model;
+            UpdateRoomCanvas();
+        }
+
+        private void mButtonUpdateView_Click(object sender, RoutedEventArgs e) {
+            if (!UpdateParameters()) {
+                mCanvas.Children.Clear();
+                return;
+            }
+
+            UpdateRoomCanvas();
+        }
+
+        private void ButtonCameraReset_Click(object sender, RoutedEventArgs e) {
+            mRoomVisualizer.ResetCamera(CAMERA_DISTANCE_DEFAULT);
+            SetupCamera();
+
+            mRoomVisualizer.Redraw();
+        }
+
+        private void mRadioButtonGenerateDefaultSpeaker_Checked(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void mRadioButtonGenerateSpeakerFromFile_Checked(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void ButtonBrowseSpeakerModel_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+    }
+}
---
+++ WWCrossFeed/WWCrossFeed/WWRoomVisualizer.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+using System.Windows.Shapes;
+
+namespace WWCrossFeed {
+    class WWRoomVisualizer {
+        private Canvas mCanvas;
+        private WWRoom mRoom;
+        private Matrix3D mWorldProjectionMatrix;
+        private float mCameraNear = 1.0f;
+        public float CameraFovHDegree { get; set; }
+        private float mCameraDistanceCurrent;
+
+        private WWVirtualTrackball mVirtualTrackball = new WWVirtualTrackball();
+
+        public WWRoomVisualizer(Canvas canvas) {
+            mCanvas = canvas;
+            mCanvas.MouseDown += mCanvas_MouseDown;
+            mCanvas.MouseMove += mCanvas_MouseMove;
+            mCanvas.MouseUp += mCanvas_MouseUp;
+            mCanvas.MouseWheel += mCanvas_MouseWheel;
+
+            mVirtualTrackball.ScreenWH = new Size(mCanvas.Width, mCanvas.Height);
+            mVirtualTrackball.SphereRadius = (float)mCanvas.Width/2;
+        }
+
+        void mCanvas_MouseWheel(object sender, MouseWheelEventArgs e) {
+            mCameraDistanceCurrent += -e.Delta * 0.01f;
+            if (mCameraDistanceCurrent < mCameraNear * 2) {
+                mCameraDistanceCurrent = mCameraNear * 2;
+            }
+            Redraw();
+        }
+
+        void mCanvas_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            mVirtualTrackball.Down(e.GetPosition(mCanvas));
+        }
+
+        void mCanvas_MouseMove(object sender, System.Windows.Input.MouseEventArgs e) {
+            if (e.LeftButton != MouseButtonState.Pressed) {
+                return;
+            }
+
+            mVirtualTrackball.Move(e.GetPosition(mCanvas));
+            Redraw();
+        }
+
+        void mCanvas_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            mVirtualTrackball.Up();
+        }
+
+        public void ResetCamera(float cameraDistance) {
+            mCameraDistanceCurrent = cameraDistance;
+            mVirtualTrackball.Reset();
+        }
+
+        private void UpdateCameraMatrix() {
+            // right-handed coordinates
+
+#if false
+            // front view
+            var eye = new Vector3D(0.0f, 0.0f, -CameraDistance);
+            var at = new Vector3D(0.0f, 0.0f, 0.0f);
+            var up = new Vector3D(0.0f, 1.0f, 0.0f);
+            var lookAt = CalculateLookAt(eye, at, up);
+#else
+            // virtual trackball
+            var cameraRot = mVirtualTrackball.RotationMatrix();
+            var cameraTranslate = new Matrix3D();
+            cameraTranslate.Translate(new Vector3D(0.0f, 0.0f, -mCameraDistanceCurrent));
+            var cameraMat = cameraTranslate * cameraRot;
+            var lookAt = cameraMat;
+            lookAt.Invert();
+#endif
+
+            var viewProjectionMatrix = CreatePerspectiveProjectionMatrix(1.0f);
+
+            mWorldProjectionMatrix = lookAt * viewProjectionMatrix;
+        }
+
+        internal static Matrix3D CalculateLookAt(Vector3D eye, Vector3D at, Vector3D up) {
+            var zaxis = (at - eye);
+            zaxis.Normalize();
+            var xaxis = Vector3D.CrossProduct(up, zaxis);
+            xaxis.Normalize();
+            var yaxis = Vector3D.CrossProduct(zaxis, xaxis);
+
+            return new Matrix3D(
+                xaxis.X, yaxis.X, zaxis.X, 0,
+                xaxis.Y, yaxis.Y, zaxis.Y, 0,
+                xaxis.Z, yaxis.Z, zaxis.Z, 0,
+                Vector3D.DotProduct(xaxis, -eye), Vector3D.DotProduct(yaxis, -eye), Vector3D.DotProduct(zaxis, -eye), 1
+                );
+        }
+
+        internal static Matrix3D CalculatePostureMatrix(Vector3D eye, Vector3D at, Vector3D up) {
+            var zaxis = (at - eye);
+            zaxis.Normalize();
+            var xaxis = Vector3D.CrossProduct(up, zaxis);
+            xaxis.Normalize();
+            var yaxis = Vector3D.CrossProduct(zaxis, xaxis);
+
+            return new Matrix3D(
+                xaxis.X, yaxis.X, zaxis.X, 0,
+                xaxis.Y, yaxis.Y, zaxis.Y, 0,
+                xaxis.Z, yaxis.Z, zaxis.Z, 0,
+                eye.X, eye.Y, eye.Z, 1.0);
+        }
+
+        private Matrix3D CreatePerspectiveProjectionMatrix(float aspectRatio) {
+            // near screen size = 2x2
+
+            float hFoV = (float)(CameraFovHDegree * Math.PI / 180.0f);
+            float zn = mCameraNear;
+            float zf = mCameraDistanceCurrent * 2.0f;
+            float xScale = (float)(1.0f / Math.Tan(hFoV / 2.0f));
+            float yScale = aspectRatio * xScale;
+            float a = (zf+zn) / (zn - zf);
+            float b = 2.0f * zn / (zn - zf);
+            return new Matrix3D(
+                    xScale, 0, 0, 0,
+                    0, yScale, 0, 0,
+                    0, 0, a, -1,
+                    0, 0, b, 0);
+        }
+
+        public void SetRoom(WWRoom room) {
+            mRoom = room;
+        }
+
+        public void Redraw() {
+            mCanvas.Children.Clear();
+
+            UpdateCameraMatrix();
+
+            RedrawRoom();
+        }
+
+        private void RedrawRoom() {
+            DrawModel(mRoom.RoomModel, Matrix3D.Identity, new SolidColorBrush(Colors.Black));
+
+            Matrix3D listenerMatrix = new Matrix3D();
+            listenerMatrix.Translate(mRoom.ListenerPos);
+            DrawModel(mRoom.ListenerModel, listenerMatrix, new SolidColorBrush(Colors.Brown));
+
+            for (int i = 0; i < WWRoom.NUM_OF_SPEAKERS; ++i) {
+                var pos = mRoom.SpeakerPos(i);
+                var dir = mRoom.SpeakerDir(i);
+                Vector3D posV = new Vector3D(pos.X, pos.Y, pos.Z);
+                Vector3D at = new Vector3D(pos.X +dir.X, pos.Y + dir.Y, pos.Z + dir.Z);
+                Vector3D up = new Vector3D(0.0f, 1.0f, 0.0f);
+
+                Matrix3D speakerMatrix = CalculatePostureMatrix(posV, at, up);
+
+                DrawModel(mRoom.SpeakerModel, speakerMatrix, new SolidColorBrush(Colors.Gray));
+            }
+        }
+
+        private void DrawModel(WW3DModel model, Matrix3D modelWorldMatrix, Brush brush) {
+            var modelProjectionMatrix = modelWorldMatrix * mWorldProjectionMatrix;
+
+            var pointArray = model.TriangleList();
+            var indexArray = model.IndexList();
+            for (int i = 0; i < indexArray.Length / 3; ++i) {
+                Point3D p0 = Point3D.Multiply(pointArray[indexArray[i * 3 + 0]], modelProjectionMatrix);
+                Point3D p1 = Point3D.Multiply(pointArray[indexArray[i * 3 + 1]], modelProjectionMatrix);
+                Point3D p2 = Point3D.Multiply(pointArray[indexArray[i * 3 + 2]], modelProjectionMatrix);
+                AddNewLine(p0, p1, brush);
+                AddNewLine(p1, p2, brush);
+                AddNewLine(p2, p0, brush);
+            }
+        }
+
+        private Vector ScaleToCanvas(Point3D v) {
+            return new Vector(mCanvas.Width/2 * (v.X+1.0f), mCanvas.Height/2 * (v.Y+1.0f));
+        }
+
+        private void AddNewLine(Point3D p0, Point3D p1, Brush brush) {
+            var from = ScaleToCanvas(p0);
+            var to = ScaleToCanvas(p1);
+
+            var line = new Line();
+            line.X1 = from.X;
+            line.Y1 = from.Y;
+            line.X2 = to.X;
+            line.Y2 = to.Y;
+            line.Stroke = brush;
+
+            mCanvas.Children.Add(line);
+        }
+    }
+}
