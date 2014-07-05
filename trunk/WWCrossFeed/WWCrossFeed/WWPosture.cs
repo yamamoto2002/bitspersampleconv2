fbb1d53fe04cb454b9c5573826283e2c3a89f693
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2014-07-05 09:24:06 +0000

    Crossfeed experiment

---
+++ trunk/WWCrossFeed/WWCrossFeed/WWPosture.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Media.Media3D;
+
+namespace WWCrossFeed {
+    class WWPosture {
+        public Point3D Pos { get; set; }
+        public Vector3D Dir { get; set; }
+
+        public WWPosture() {
+            Pos = new Point3D();
+            Dir = new Vector3D(0.0f, 0.0f, 1.0f);
+        }
+
+        public WWPosture(Point3D pos, Vector3D dir) {
+            Pos = pos;
+            Dir = dir;
+        }
+    }
+}
---
+++ trunk/WWCrossFeed/WWCrossFeed/WWRoom.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Media.Media3D;
+
+namespace WWCrossFeed {
+    class WWRoom {
+        public const int NUM_OF_SPEAKERS = 2;
+        public WW3DModel ListenerModel { get; set; }
+        public WW3DModel SpeakerModel { get; set; }
+        public WW3DModel RoomModel { get; set; }
+        private WWPosture[] mSpeakerPos = new WWPosture[NUM_OF_SPEAKERS];
+        public Vector3D ListenerPos { get; set; }
+
+        public WWRoom() {
+            for (int i=0; i<NUM_OF_SPEAKERS; ++i) {
+                mSpeakerPos[i] = new WWPosture();
+            }
+        }
+
+        public Point3D SpeakerPos(int idx) {
+            return mSpeakerPos[idx].Pos;
+        }
+        public Vector3D SpeakerDir(int idx) {
+            return mSpeakerPos[idx].Dir;
+        }
+
+        public void SetSpeakerPos(int idx, Point3D pos) {
+            mSpeakerPos[idx].Pos = pos;
+        }
+
+        public void SetSpeakerDir(int idx, Vector3D dir) {
+            mSpeakerPos[idx].Dir = dir;
+        }
+    }
+}
