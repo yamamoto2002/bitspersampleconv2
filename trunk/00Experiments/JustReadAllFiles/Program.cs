fd2ed72a8d10ccf0abde02706975501fc4b22388
yamamoto2002 <yamamoto2002@7bb0643a-f146-11dd-a295-0b7b0935bf94>
2014-12-29 19:43:55 +0000

    JustReadAllFiles display read performance

--- trunk/00Experiments/JustReadAllFiles/Program.cs
+++ trunk/00Experiments/JustReadAllFiles/Program.cs
@@ -7,38 +7,49 @@ namespace JustReadAllFiles {
     class Program {
         const int BUFF_BYTES = 1048576;
 
-        static void ReadAllFilesOnDirectory(DirectoryInfo di) {
+        static long ReadAllFilesOnDirectory(DirectoryInfo di) {
+            long totalBytes = 0;
+            
             var fArray = di.GetFiles();
 
             Parallel.For(0, fArray.Length, i => {
                 try {
                     var f = fArray[i];
                     var fs = f.OpenRead();
+                    long readBytes = 0;
                     int bytes = 0;
                     byte[] b = new byte[BUFF_BYTES];
                     do {
                         bytes = fs.Read(b, 0, BUFF_BYTES);
+                        readBytes += bytes;
                     } while (bytes == BUFF_BYTES);
+                    System.Threading.Interlocked.Add(ref totalBytes, readBytes);
+
                     //Console.WriteLine("    {0}", f.Name);
                 } catch (Exception ex) {
                     //Console.WriteLine("ReadAllFilesOnDirectory {0}", ex);
                 }
             });
+
+            return totalBytes;
         }
 
-        static void DirectoryScanRecursive(DirectoryInfo di) {
-            ReadAllFilesOnDirectory(di);
+        static long DirectoryScanRecursive(DirectoryInfo di) {
+            long totalBytes = ReadAllFilesOnDirectory(di);
 
             var dArray = di.GetDirectories();
             Parallel.For(0, dArray.Length, i => {
                 try {
                     var subd = dArray[i];
                     Console.WriteLine("  {0}", subd.FullName);
-                    DirectoryScanRecursive(subd);
+                    long bytes = DirectoryScanRecursive(subd);
+                    System.Threading.Interlocked.Add(ref totalBytes, bytes);
                 } catch (Exception ex) {
                     //Console.WriteLine("DirectoryScanRecursive {0}", ex);
                 }
             });
+
+            return totalBytes;
         }
 
         static void Main(string[] args) {
@@ -59,10 +70,10 @@ namespace JustReadAllFiles {
             var sw = new Stopwatch();
             sw.Start();
 
-            DirectoryScanRecursive(di);
+            long totalBytes = DirectoryScanRecursive(di);
 
             sw.Stop();
-            Console.WriteLine("Elapsed time: {0}", sw.Elapsed);
+            Console.WriteLine("Elapsed time: {0}, total read bytes={1}, {2}MB/s", sw.Elapsed, totalBytes, (double)totalBytes / (sw.ElapsedMilliseconds/1000.0) / 1000.0/1000.0);
         }
     }
 }
